8a9825c39748fc896f8911bef950bb3403e0d550
bobbyhadz <opmftw@gmail.com>
2024-04-07 08:40:28 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div style={{border: '1px solid red'}}>bobbyhadz.com</div>
+
+      <br />
+
+      <div style={{border: '1px dashed red'}}>bobbyhadz.com</div>
+
+      <br />
+
+      <div style={{border: '1px solid rgba(0,255,0,0.3)'}}>
+        bobbyhadz.com
+      </div>
+
+      <br />
+
+      <div style={{border: '2px dotted red'}}>bobbyhadz.com</div>
+
+      <br />
+
+      <div style={{borderBottom: '2px dotted red'}}>
+        bobbyhadz.com
+      </div>
+
+      <br />
+      <br />
+
+      <div style={{borderTop: '2px dotted red'}}>
+        bobbyhadz.com
+      </div>
+    </div>
+  );
+};
+
+export default App;
